fc9321000ebe2fb1da3adc0b91fb00794ffab927
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-07-05 14:57:16 +0530

    new project

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,103 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
 
-export default function Home() {
+const page = () => {
+  const [overs, setOvers] = useState<number>(5)
+  const [over, setOver] = useState<number>(0)
+  const [runs, setRuns] = useState<number>(0)
+  const [wickets, setWickets] = useState<number>(0)
+  const selectOvers = () => {
+    if (overs > 20) {
+      alert('hosh me over rakho rat ho jaegi khelete khelte')
+    }
+    else {
+      localStorage.setItem('over', JSON.stringify(over))
+      setOver(overs*6)
+    }
+  }
+  const single = () => {
+    setRuns(runs+1)
+    setOver(over-1)
+  }
+  const double = () => {
+    setRuns(runs+2)
+    setOver(over-1)
+  }
+  const triple = () => {
+    setRuns(runs+3)
+    setOver(over-1)
+  }
+  const four = () => {
+    setRuns(runs+4)
+    setOver(over-1)
+  }
+  const five = () => {
+    setRuns(runs+5)
+    setOver(over-1)
+  }
+  const six = () => {
+    setRuns(runs+6)
+    setOver(over-1)
+  }
+  const wicket = () => {
+    setWickets(wickets+1)
+    setOver(over-1)
+  }
+  const extra = () => {
+    setRuns(runs+1)
+  }
+  const closeGame=()=>{
+    localStorage.removeItem('overs')
+    setOver(0)
+    setOvers(0)
+    setRuns(0)
+    setWickets(0)
+  }
+  if(over===1){
+    closeGame()
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className='w-full h-screen flex items-center flex-col'>
+      <div className='relative top-0 text-[40px]'>cricscorer</div>
+      <div id="input" className='flex'>
+        <input type="number" placeholder='overs?' className='border p-2 w-[100px]'
+          value={overs}
+          onChange={(e) => setOvers(parseInt(e.target.value))}
         />
+        <button className='bg-slate-600 p-2
+        '
+          onClick={selectOvers}
+        >
+          Start Game</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="onball" className='w-[70%] h-[100px] flex justify-between items-center flex-wrap mt-9'>
+        <button className='w-[80px] bg-green-300 p-1 text-[12px] text-[white] rounded'
+        onClick={single}>single</button>
+        <button className='w-[80px] bg-green-400 p-1 text-[12px] text-[white] rounded'
+        onClick={double}>double</button>
+        <button className='w-[80px] bg-green-500 p-1 text-[12px] text-[white] rounded'
+        onClick={triple}>triple</button>
+        <button className='w-[80px] bg-green-600 p-1 text-[12px] text-[white] rounded'
+        onClick={four}>four</button>
+        <button className='w-[80px] bg-green-700 p-1 text-[12px] text-[white] rounded'
+        onClick={five}>five</button>
+        <button className='w-[80px] bg-green-800 p-1 text-[12px] text-[white] rounded'
+        onClick={six}>six</button>
+        <button className='w-[80px] bg-red-900 p-1 text-[12px] text-[white] rounded'
+        onClick={wicket}>wicket</button>
+        <button className='w-[80px] bg-green-900 p-1 text-[12px] text-[white] rounded'
+        onClick={extra}>wide or nb</button>
       </div>
-    </main>
-  );
+      <div id="lekhajokha">
+      </div>
+      <div id="report">
+        <h1>Runs:{runs}</h1>
+        <h1>Wicket:{wickets}</h1>
+        <h1>Balls left:{over}</h1>
+      </div>
+      <button className='bg-blue-600 p-2 rounded m-8'>Close the Game</button>
+    </div>
+  )
 }
+
+export default page
